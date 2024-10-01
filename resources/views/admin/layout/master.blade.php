5248fa4c9af8df06c4fdd3248c535751ab4b7965
Your Name <tamndph41864@fpt.edu.vnvn>
2024-10-01 16:39:35 +0700

    Admin - Setup Mastering (Part 2)

--- resources/views/admin/layout/master.blade.php
+++ resources/views/admin/layout/master.blade.php
@@ -10,12 +10,6 @@
     <link rel="stylesheet" href="{{ asset('admin/modules/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('admin/modules/fontawesome/css/all.min.css') }}">
 
-    <!-- CSS Libraries -->
-    <link rel="stylesheet" href="{{ asset('admin/modules/jqvmap/dist/jqvmap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('admin/modules/summernote/summernote-bs4.css') }}">
-    <link rel="stylesheet" href="{{ asset('admin/modules/owlcarousel2/dist/assets/owl.carousel.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('admin/modules/owlcarousel2/dist/assets/owl.theme.default.min.css') }}">
-
     <!-- Template CSS -->
     <link rel="stylesheet" href="{{ asset('admin/css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('admin/css/components.css') }}">
@@ -69,13 +63,6 @@
     <script src="{{ asset('admin/modules/moment.min.js') }}"></script>
     <script src="{{ asset('admin/js/stisla.js') }}"></script>
 
-    <!-- JS Libraies -->
-    <script src="{{ asset('admin/modules/jquery.sparkline.min.js') }}"></script>
-    <script src="{{ asset('admin/modules/chart.min.js') }}"></script>
-    <script src="{{ asset('admin/modules/owlcarousel2/dist/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('admin/modules/summernote/summernote-bs4.js') }}"></script>
-    <script src="{{ asset('admin/modules/chocolat/dist/js/jquery.chocolat.min.js') }}"></script>
-
     <!-- Page Specific JS File -->
     <script src="{{ asset('admin/js/page/index.js') }}"></script>
 
@@ -83,5 +70,6 @@
     <script src="{{ asset('admin/js/scripts.js') }}"></script>
     <script src="{{ asset('admin/js/custom.js') }}"></script>
 </body>
+
 
 </html>
